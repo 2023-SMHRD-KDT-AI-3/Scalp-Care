ab81f88a2124d70c25547ecc5dd60d1779dd6191
smhrd2 <brown407@naver.com>
2023-12-14 16:30:38 +0900

    주석2

--- app/src/main/java/com/example/applicationscalp_care/MainActivity.java
+++ app/src/main/java/com/example/applicationscalp_care/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 주석
+        // 주석2
 
     }
 }
