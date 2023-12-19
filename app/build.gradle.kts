a679cf803c3c8674023967a9e4e952f38ea7f668
Scalp_care <whsworka135@naver.com>
2023-12-19 14:17:12 +0900

    test111

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,9 @@ android {
     namespace = "com.example.applicationscalp_care"
     compileSdk = 34
 
-    viewBinding { enable = true }
+    viewBinding {
+        enable = true
+    }
 
     defaultConfig {
         applicationId = "com.example.applicationscalp_care"
@@ -31,7 +33,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.android.volley:volley:1.2.1") // 네트워크 통신 라이브러리 추가
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/applicationscalp_care/MainActivity.java
+++ app/src/main/java/com/example/applicationscalp_care/MainActivity.java
@@ -2,22 +2,68 @@ package com.example.applicationscalp_care;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+
+import com.android.volley.RequestQueue;
 import com.example.applicationscalp_care.databinding.ActivityMainBinding;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
 
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // 주석
-        // 주석2
-        // 주석3
-        // 주석4
-        //test
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // test
+        binding.fl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               StringRequest request = new StringRequest(
+                       Request.Method.POST,
+                       "http://172.30.1.54:8080/login",
+                       new Response.Listener<String>() {
+                           @Override
+                           public void onResponse(String response) {
+                               Log.d("testtest", response);
+                           }
+                       },
+                       new Response.ErrorListener() {
+                           @Override
+                           public void onErrorResponse(VolleyError error) {
+
+                           }
+                       }
+               ){
+                   protected Map<String, String> getParams() throws AuthFailureError{
+                       Map<String, String> params = new HashMap<>();
+                       params.put("1","2");
+                       params.put("1","2");
+
+                       return params;
+
+                   }
+               };
+                queue.add(request);
+            }
+        });
+
     }
 }
