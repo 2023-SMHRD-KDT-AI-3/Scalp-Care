3db061a9caa9579f9030ba9cf52a67f806e35cdd
jrjosh1207 <jrjosh1207@gmail.com>
2023-12-26 11:27:27 +0900

    커밋

--- app/src/main/java/com/example/applicationscalp_care/MainActivity.java
+++ app/src/main/java/com/example/applicationscalp_care/MainActivity.java
@@ -27,46 +27,12 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        // test
-        binding.fl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               StringRequest request = new StringRequest(
-                       Request.Method.POST,
-                       "http://172.30.1.54:8080/login",
-                       new Response.Listener<String>() {
-                           @Override
-                           public void onResponse(String response) {
-                               Log.d("testtest", response);
-                           }
-                       },
-                       new Response.ErrorListener() {
-                           @Override
-                           public void onErrorResponse(VolleyError error) {
-
-                           }
-                       }
-               ){
-                   protected Map<String, String> getParams() throws AuthFailureError{
-                       Map<String, String> params = new HashMap<>();
-                       params.put("1","2");
-                       params.put("1","2");
-
-                       return params;
-
-                   }
-               };
-                queue.add(request);
-            }
-        });
 
         // 실행 할 때 바로 홈 화면 나올 수 있게
         getSupportFragmentManager().beginTransaction().replace(R.id.fl, new HomeFragment()).commit();
