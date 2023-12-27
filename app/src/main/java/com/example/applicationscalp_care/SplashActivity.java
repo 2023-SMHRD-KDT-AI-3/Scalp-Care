258543ed48b14870e32dccd2725f0d3a2d3267c7
Scalp_care <whsworka135@naver.com>
2023-12-27 14:44:02 +0900

    로그인 기능 완료(아마도?)

--- app/src/main/java/com/example/applicationscalp_care/SplashActivity.java
+++ app/src/main/java/com/example/applicationscalp_care/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.applicationscalp_care;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -15,10 +17,21 @@ public class SplashActivity extends AppCompatActivity {
 
         // 3초 동안 스플래시 화면을 표시한 후 메인 액티비티로 이동
         new Handler().postDelayed(new Runnable() {
+
+            SharedPreferences autoLogin = getSharedPreferences("autoLogin", Context.MODE_PRIVATE);
+            String uid = autoLogin.getString("uid","null");
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
+                // 로그인이 안되어 있으면 LoginActivity로 이동
+                if(uid.equals("null")) {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+
+                // 로그인이 되어 있으면 MainActivity로 이동
+                }else{
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
                 finish();
             }
         }, 3000);
