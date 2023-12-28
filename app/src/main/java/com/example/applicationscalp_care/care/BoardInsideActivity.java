443783af58e8f86b9c5125cc2736f017aeb9c7bf
smhrd2 <brown407@naver.com>
2023-12-28 09:42:00 +0900

    BoardInsideActivity 만드는중

--- app/src/main/java/com/example/applicationscalp_care/care/BoardInsideActivity.java
+++ app/src/main/java/com/example/applicationscalp_care/care/BoardInsideActivity.java
@@ -2,15 +2,28 @@ package com.example.applicationscalp_care.care;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.applicationscalp_care.R;
+import com.example.applicationscalp_care.databinding.ActivityBoardInsideBinding;
 
 public class BoardInsideActivity extends AppCompatActivity {
 
+    private ActivityBoardInsideBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_board_inside);
+        binding = ActivityBoardInsideBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+//        Intent data = getIntent();
+//
+//        String indate = data.getStringExtra("indate");
+//        String content = data.getStringExtra("content");
+//
+//        binding.tvInsideTime.setText(indate);
+//        binding.tvInsideContent.setText(content);
     }
 }
