7af8897476778fcd3e99337b53be99712df1a027
smhrd2 <brown407@naver.com>
2023-12-29 09:33:22 +0900

    지도기능

--- app/src/main/java/com/example/applicationscalp_care/LoginActivity.java
+++ app/src/main/java/com/example/applicationscalp_care/LoginActivity.java
@@ -124,9 +124,13 @@ public class LoginActivity extends AppCompatActivity {
 
     private void insertLogin(String uid, String name, String classes, String email){
         Log.d("LoginActivity","DB저장 함수 실행");
+        Log.d("qwer",uid);
+        Log.d("qwer",name);
+        Log.d("qwer",classes);
+        Log.d("qwer",email);
         StringRequest request = new StringRequest(
                 Request.Method.POST,
-                "http://192.168.219.50:8089/join",
+                "http://192.168.219.52:8089/join",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -156,6 +160,18 @@ public class LoginActivity extends AppCompatActivity {
         };
         queue.add(request);
     }
+
+
+
+
+
+
+
+
+
+
+
+
 
     private void updateKakaoLoginUi() {
 
