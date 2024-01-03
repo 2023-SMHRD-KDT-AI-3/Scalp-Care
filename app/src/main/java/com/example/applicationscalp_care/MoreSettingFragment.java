e689c8d7f102a74087ff2a62c88c25709d816a47
jrjosh1207 <jrjosh1207@gmail.com>
2024-01-03 09:13:16 +0900

    커밋

--- app/src/main/java/com/example/applicationscalp_care/MoreSettingFragment.java
+++ app/src/main/java/com/example/applicationscalp_care/MoreSettingFragment.java
@@ -8,30 +8,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
-import com.example.applicationscalp_care.databinding.ActivityTeamSogaeBinding;
-import com.example.applicationscalp_care.databinding.FragmentCareBinding;
 import com.example.applicationscalp_care.databinding.FragmentMoreSettingBinding;
+import com.example.applicationscalp_care.moresetting.TeamSogaeActivity;
 import com.kakao.sdk.user.UserApiClient;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
