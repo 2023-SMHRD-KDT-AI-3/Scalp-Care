06c9c2a00d3ddd3f23b80f9f7d70d7637af038f3
Scalp_care <whsworka135@naver.com>
2024-01-05 15:54:08 +0900

    두피 사진 비교 작성중

--- app/src/main/java/com/example/applicationscalp_care/care/BoardAdapter.java
+++ app/src/main/java/com/example/applicationscalp_care/care/BoardAdapter.java
@@ -1,6 +1,9 @@
 package com.example.applicationscalp_care.care;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
@@ -23,6 +26,7 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardViewHolder> {
     private ArrayList<BoardVO> dataset;
     private ArrayList<String> keyset;
 
+
     public BoardAdapter(ArrayList<BoardVO> dataset, ArrayList<String> keyset) {
         // 초기화
         this.dataset = dataset;
@@ -32,16 +36,13 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardViewHolder> {
     @NonNull
     @Override // gaesigul_board.xml을 객체화 시켜 화면에 출력하는 기능 → BoardViewHolder한테 보냄
     public BoardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.gaesigul_board, parent, false);
-        BoardViewHolder holder = new BoardViewHolder(view);
+        View view1 = LayoutInflater.from(parent.getContext()).inflate(R.layout.gaesigul_board, parent, false);
+        BoardViewHolder holder = new BoardViewHolder(view1);
         return holder;
     }
 
     @Override //  RecyclerView의 각 항목에 대한 데이터를 결합하고 해당 데이터를 화면에 표시하는 기능
     public void onBindViewHolder(@NonNull BoardViewHolder holder, int position) {
-//        String indate = dataset.get(position).getIndate();
-//        String content = dataset.get(position).getContent();
-//        String uid = dataset.get(position).getUid();
 
         BoardVO vo = dataset.get(position);
 
@@ -51,17 +52,36 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardViewHolder> {
         holder.listener = new BoardItemListener() {
             @Override
             public void BoardClickListener(View v, int position) {
-                Intent intent = new Intent(v.getContext(), BoardInsideActivity.class);
+                Intent intent ;
 
-                intent.putExtra("indate", vo.getIndate());
-                intent.putExtra("content", vo.getContent());
-                intent.putExtra("ucNum",vo.getUc_num());
+                SharedPreferences nowPage = v.getContext().getSharedPreferences("page", Context.MODE_PRIVATE);
+                String page = nowPage.getString("page","null");
+                Log.d("Adapter에서 확인",page);
 
 
-                v.getContext().startActivity(intent);
+                if(page.equals("care")){
+                    intent = new Intent(v.getContext(),BoardInsideActivity.class);
+                    intent.putExtra("indate", vo.getIndate());
+                    intent.putExtra("content", vo.getContent());
+                    intent.putExtra("ucNum",vo.getUc_num());
+                    v.getContext().startActivity(intent);
+
+                }else if(page.equals("compare")){
+                    intent = new Intent();
+                    intent.putExtra("indate", vo.getIndate());
+                    intent.putExtra("content", vo.getContent());
+                    intent.putExtra("ucNum",vo.getUc_num());
+
+                    ((Activity)v.getContext()).setResult(Activity.RESULT_OK,intent);
+                    ((Activity) v.getContext()).finish();
+
+                }
+
+
             }
         };
     }
+
 
     @Override
     public int getItemCount() { return dataset.size(); }
--- app/src/main/java/com/example/applicationscalp_care/care/ScalpCompareActivity.java
+++ app/src/main/java/com/example/applicationscalp_care/care/ScalpCompareActivity.java
@@ -9,6 +9,7 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -50,8 +51,8 @@ public class ScalpCompareActivity extends AppCompatActivity {
             new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
-                    Log.d("CareActivity11","다시 데이터 가져오기");
-                    //getBoardData();
+                    String indate = result.getData().getStringExtra("indate");
+                    binding.indate1.setText(indate);
 
                 }
             }
@@ -63,6 +64,12 @@ public class ScalpCompareActivity extends AppCompatActivity {
 
         binding = ActivityScalpCompareBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        SharedPreferences nowPage = getSharedPreferences("page", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = nowPage.edit();
+        editor.putString("page","compare");
+        editor.commit();
+
 
         // 뒤로가기
         binding.imgBack3.setOnClickListener(v -> {
@@ -76,6 +83,7 @@ public class ScalpCompareActivity extends AppCompatActivity {
         binding.imgContent2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("클릭이벤트 실행","실행확인");
                 Intent intent = new Intent(ScalpCompareActivity.this, BoardRvActivity.class);
                 BoardRvLauncher.launch(intent);
 
